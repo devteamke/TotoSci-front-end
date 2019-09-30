9569f8e48b0c60a59c0785df4657d25c57f65f20
devteamke <devteamke2018@gmail.com>
2019-09-30 04:37:09 +0000

    whatevver

--- src/constants/Globals.js
+++ src/constants/Globals.js
@@ -1,6 +1,6 @@
 const globals = {
   BASE_URL: 'http://35.246.149.29:1111',
   //BASE_URL:'https://schoolsystem.run.goorm.io:8080'
-  PUBLIC_URL: 'http://35.246.149.29:3000'
+  PUBLIC_URL: 'https://3000-a9875cbd-7eb1-4094-a001-4f4e349ac418.ws-eu0.gitpod.io/'
 };
 export default globals;
